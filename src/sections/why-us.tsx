5de0245df5deb64a33646778bed65ad957c4dae6
Icy-Tequila <janeilletrixiedeleon@gmail.com>
2025-09-08 18:55:05 +0800

    Chore: implement responsiveness in Why us section

--- src/sections/why-us.tsx
+++ src/sections/why-us.tsx
@@ -3,68 +3,67 @@ import Image from "next/image";
 
 export default function WhyUs() {
   return (
-    <div className="my-15">
-      <div className="garamond text-5xl text-center mt-7 mb-12">
-        Truly, <span className="text-[#ff5100]">Indian</span>
+    <div className="my-15 flex flex-col items-center">
+      <div className="w-[90%] lg:w-[80%] flex flex-row md:flex-none md:justify-between">
+        <div className="garamond text-5xl md:text-6xl text-[#1e2339] text-center md:text-start mt-7 mb-12">
+          Truly, <span className="text-[#ff5100]">Indian.</span>
+        </div>
+        <div className="baloo-2 text-lg text-center md:max-w-[400px] md:text-start mt-7 mb-12 mx-5">
+          There&apos;s only one way to revolutionize India&apos;s coffee
+          landscape — by bringing back the authentic Indian filter coffee.
+          That&apos;s exactly what we&apos;re doing through our growing coffee
+          cafe franchise in India. As a leading coffee shop, we are committed to
+          preserving tradition while creating a modern coffee experience for
+          every coffee lover.
+        </div>
       </div>
-      <div className="baloo-2 text-lg text-center mt-7 mb-12 mx-5">
-        There&apos;s only one way to revolutionize India&apos;s coffee landscape
-        — by bringing back the authentic Indian filter coffee. That&apos;s
-        exactly what we&apos;re doing through our growing coffee cafe franchise
-        in India. As a leading coffee shop, we are committed to preserving
-        tradition while creating a modern coffee experience for every coffee
-        lover.
-      </div>
-      <div className="flex flex-wrap justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-3 md:gap-[40px] w-[90%]">
         <div className="flex flex-col items-center mb-10">
-          <div className="w-60 h-70 max-w-sm border-30 border-b-0 border-[#ffe8de] rounded-t-full rounded-b-none overflow-hidden">
+          <div className="w-60 md:w-full h-70 border-30 border-b-0 border-[#ffe8de] rounded-t-full rounded-b-none overflow-hidden relative">
             <Image
               src="/images/story-beans.avif"
-              width={400}
-              height={400}
+              fill
               alt="Framed"
-              className="w-full h-auto object-cover"
+              className="object-cover"
             />
           </div>
-          <div className="baloo-2 text-lg text-center mt-2">
+          <div className="baloo-2 text-lg text-center my-2">
             Specially Sourced Coffee Beans
           </div>
-          <div className="baloo-2 text-center text-sm text-gray-400">
+          <div className="baloo-2 text-center text-sm text-gray-500">
             Single origin to Story Beans®
           </div>
         </div>
         <div className="flex flex-col items-center mb-10">
-          <div className="w-60 h-70 max-w-sm border-25 border-b-0 border-[#ffe8de] rounded-t-full rounded-b-none overflow-hidden">
+          <div className="w-60 md:w-full h-70 border-25 border-b-0 border-[#ffe8de] rounded-t-full rounded-b-none overflow-hidden relative">
             <Image
               src="/images/indian-latitude.avif"
-              width={400}
-              height={400}
+              fill
               alt="Framed"
-              className="w-full h-auto object-cover"
+              className="object-cover"
             />
           </div>
-          <div className="baloo-2 text-lg text-center mt-2">
-            Specially Sourced Coffee Beans
+          <div className="baloo-2 text-lg text-center my-2">
+            South and North Snacks
           </div>
-          <div className="baloo-2 text-center text-sm text-gray-400">
-            Single origin to Story Beans®
+          <div className="baloo-2 text-center text-sm text-gray-500">
+            Snacks of Indian Latitude
           </div>
         </div>
-        <div className="flex flex-col items-center">
-          <div className="w-60 h-70 max-w-sm border-30 border-b-0 border-[#ffe8de] rounded-t-full rounded-b-none overflow-hidden">
+        <div className="flex flex-col items-center mb-10">
+          <div className="w-60 md:w-full h-70  border-30 border-b-0 border-[#ffe8de] rounded-t-full rounded-b-none overflow-hidden relative">
             <Image
               src="/images/incredible-india.avif"
-              width={400}
-              height={400}
+              fill
               alt="Framed"
-              className="w-full h-auto object-cover"
+              className="object-cover"
             />
           </div>
-          <div className="baloo-2 text-lg text-center mt-2">
-            Specially Sourced Coffee Beans
+          <div className="baloo-2 text-lg text-center my-2">
+            Incredible Flavours
           </div>
-          <div className="baloo-2 text-center text-sm text-gray-400">
-            Single origin to Story Beans®
+          <div className="baloo-2 text-center text-sm text-gray-500">
+            Originated from Incredible India
           </div>
         </div>
       </div>
